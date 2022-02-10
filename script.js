2970c4b65ebd2e00f704e7cfb4ec4e614300a8e5
asendereva <43315369+asendereva@users.noreply.github.com>
2022-02-10 11:43:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,17 @@
-var $speakerCarousel = $(".speaker-carousel");
-var $readersLogo = $(".readers-logo");
-var $confReviews = $(".conf-reviews");
 
-  $speakerCarousel.slick({
-    dots: true,
-    arrows:true,
-    infinite: true,
-    centerPadding: '20px',
-    slidesToShow: 2,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          centerMode: true,
-          centerPadding: '20px',
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          dots: false,
-          centerMode: true,
-          centerPadding: '10px',
-          slidesToShow: 1
-        }
-      }
-    ]
-    });
-
-
-  $confReviews.slick({
+$(".speaker-carousel").slick({
   dots: true,
+  arrows:true,
   infinite: true,
-  centerMode: true,
   centerPadding: '20px',
-  slidesToShow: 3,
+  slidesToShow: 2,
   responsive: [
     {
       breakpoint: 768,
       settings: {
-        arrows: true,
         centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
+        centerPadding: '60px',
+        slidesToShow: 1
       }
     },
     {
@@ -53,7 +20,7 @@ var $confReviews = $(".conf-reviews");
         arrows: true,
         dots: false,
         centerMode: true,
-        centerPadding: '40px',
+        centerPadding: '10px',
         slidesToShow: 1
       }
     }
@@ -61,36 +28,68 @@ var $confReviews = $(".conf-reviews");
   });
 
 
-  if($(window).width() < 768){ 
-    $(".readers-logo").slick({ 
-      dots: false,
-      infinite: true,
+  $(".conf-reviews").slick({
+dots: true,
+infinite: true,
+centerMode: true,
+centerPadding: '20px',
+slidesToShow: 3,
+responsive: [
+  {
+    breakpoint: 768,
+    settings: {
+      arrows: true,
       centerMode: true,
-      centerPadding: '20px',
-      slidesToShow: 4,
-      responsive: [
-           {
-          breakpoint: 480,
-          settings: {
-            arrows: true,
-            dots: false,
-            centerMode: true,
-            centerPadding: '20px',
-            slidesToShow: 3
-          }
-        }
-      ]
-   }); 
-    
-  }else{ 
-    $removeClass(".readers-logo"); 
+      centerPadding: '40px',
+      slidesToShow: 3
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      arrows: true,
+      dots: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1
+    }
   }
+]
+});
 
- /*collapse questions*/
- $('.questions .item .collapse').click(function () {
-  $(this).closest('.questions .item').find('.open').slideToggle();
-  $(this).toggleClass('opened');
+
+      $(".readers-logo").slick({ 
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 4,
+        mobileFirst: true,
+        responsive: [
+             {
+            breakpoint: 325,
+            settings: {
+              arrows: true,
+              dots: false,
+              centerMode: true,
+              centerPadding: '20px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: "unslick"
+         }           
+        ]
+     }); 
+      
+
+
+
+/*collapse questions*/
+$('.questions .item .collapse').click(function () {
+$(this).closest('.questions .item').find('.open').slideToggle();
+$(this).toggleClass('opened');
 })
-
 
 
